a9289aa94ec80cd6f533bb6679a2ea490c48fc36
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-05-23 17:30:27 +0530

    navbar enhancements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,32 +2,30 @@ import logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (
-    <div className="sticky top-0 z-50 w-full bg-base-200">
-      <div className="du-navbar mx-auto du-rounded-box bg-base-100 max-w-screen-lg">
-        {/* Change this */}
-        {/* logo */}
-        <div className="flex-1">
-          <a href="https://www.google.com">
-            <img src={logo} alt="logo" className="inline aspect-auto h-10" />
-          </a>
-        </div>
+    <div className="du-navbar mx-auto du-rounded-box bg-base-100 max-w-screen-lg sticky top-0 z-50">
+      {/* Change this */}
+      {/* logo */}
+      <div className="flex-1">
+        <a href="https://www.google.com">
+          <img src={logo} alt="logo" className="inline aspect-auto h-10" />
+        </a>
+      </div>
 
-        <div className="flex-none">
-          <ul className="du-menu du-menu-horizontal px-1">
-            <li>
-              <a>About Me</a>
-            </li>
-            <li>
-              <a>My Skills</a>
-            </li>
-            <li>
-              <a>Projects</a>
-            </li>
-            <li>
-              <a>Contact</a>
-            </li>
-          </ul>
-        </div>
+      <div className="flex-none">
+        <ul className="du-menu du-menu-horizontal px-1">
+          <li>
+            <a>About Me</a>
+          </li>
+          <li>
+            <a>My Skills</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
