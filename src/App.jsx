b19fb02a5602576452b75106407f63bc2812e8b1
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-05-24 20:41:28 +0530

    display changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import DarkThemeComponent from "./components/DarkThemeComponent";
 import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Navbar />
         <Hero />
         <DarkThemeComponent child={<About />}></DarkThemeComponent>
+        <Skills />
+        <DarkThemeComponent />
       </div>
     </>
   );
