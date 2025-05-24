e5f18a09632c1f03740d40707817b135f48dc3a7
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-05-24 23:48:46 +0530

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.svg";
 
 export default function Navbar() {
   return (
-    <div className="du-navbar mx-auto rounded-full bg-base-100/60 max-w-screen-lg sticky top-2 z-50 backdrop-blur-xs">
+    <div className="du-navbar mx-auto rounded-full bg-base-100/60 max-w-screen-lg sticky top-2 z-50 backdrop-blur-sm">
       {/* Change this */}
       {/* logo */}
       <div className="flex-1">
