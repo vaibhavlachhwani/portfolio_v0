27428a15880eabff7fca9d76cdc4407abd8b18fb
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-05-25 14:25:02 +0530

    add footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import logo from "../assets/images/logo/logo-light.svg";
-
+import linkedin from "../assets/images/social/icon-linkedin.svg";
+import github from "../assets/images/social/icon-github.svg";
+import school from "../assets/images/social/icon-school.svg";
+import instagram from "../assets/images/social/icon-instagram.svg";
 
 export default function Footer() {
   return (
     <>
-      <div className="w-full aspect-[4.5/1] bg-[url('src/assets/waveFooter.svg')] bg-no-repeat bg-cover bg-top"></div>
+      <div className="w-full aspect-[4.5/1] bg-[url('src/assets/images/wave_dividers/waveFooter.svg')] bg-no-repeat bg-cover bg-top"></div>
 
       <div data-theme="night">
         <footer className="du-footer mx-auto max-w-screen-lg du-footer-horizontal du-footer-center bg-base-100 text-base-content p-10">
@@ -19,30 +22,33 @@ export default function Footer() {
           </aside>
           <nav>
             <div className="grid grid-flow-col gap-4">
-              <a>
-                
+              <a
+                href="https://www.linkedin.com/in/vaibhavlachhwani/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={linkedin} className="size-6" alt="" />
               </a>
-              <a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                </svg>
+              <a
+                href="https://github.com/vaibhavlachhwani"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={github} className="size-6" alt="" />
               </a>
-              <a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                </svg>
+              <a
+                href="https://iiitkota.ac.in/student/20231011"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={school} className="size-6" alt="" />
+              </a>
+              <a
+                href="https://www.instagram.com/vaibhavlachhwani/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={instagram} className="size-6" alt="" />
               </a>
             </div>
           </nav>
