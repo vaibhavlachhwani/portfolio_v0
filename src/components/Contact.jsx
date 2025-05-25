97d3b29758596d8c53986d1b5a836993e655a844
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-05-25 13:29:35 +0530

    add Contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,25 @@
+export default function Contact() {
+  return (
+    <section id="contact">
+      <div className="du-hero bg-base-200">
+        <div className="du-hero-content text-center">
+          <div className="max-w-lg">
+            <h1 className="text-5xl font-bold">Get In Touch</h1>
+            <p className="py-6">
+              Have something in mind? Whether it’s a challenging project, a
+              collaboration opportunity, or a casual chat about technology and
+              innovation, I’m always up for connecting. Let’s bring ideas to
+              life and make an impact together!
+            </p>
+            <a
+              href="mailto:vaibhav.lachhwani111@gmail.com"
+              className="du-btn du-btn-wide du-btn-outline du-btn-primary"
+            >
+              Say Hello! 👋
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
