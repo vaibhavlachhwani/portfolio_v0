5b07b6695a7fdfc449dd8312a0c2b877357ab16c
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-05-26 20:15:19 +0530

    make section headers responsive

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,5 +1,7 @@
 export function SectionTitle({ text }) {
   return (
-    <h2 className="font-bold text-center w-max text-5xl mx-auto">{text}</h2>
+    <h2 className="font-bold text-center w-max text-3xl lg:text-5xl mx-auto">
+      {text}
+    </h2>
   );
 }
