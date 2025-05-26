e556c1bf3a92feb9cbf29f89ce79d70052fa6dbc
vaibhavlachhwani <vaibhav.lachhwani111@gmail.com>
2025-05-26 17:45:24 +0530

    display changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import DarkThemeComponent from "./components/DarkThemeComponent";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
@@ -8,12 +8,14 @@ import Projects from "./components/projects/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import DarkThemeComponentAlt from "./components/DarkThemeComponentAlt";
+import NavbarMobile from "./components/navbar/NavbarMobile";
 
 function App() {
   return (
     <>
       <div className="bg-base-200">
         <Navbar />
+        <NavbarMobile />
         <Hero />
         <DarkThemeComponent child={<About />} />
         <Skills />
